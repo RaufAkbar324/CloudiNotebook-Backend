ce11c6b2cc463f0e94a64648757872a9144ddadf
RaufAkbar324 <raufakbar324@gmail.com>
2026-03-28 07:56:12 +0500

    Initial commit

---
+++ db.js
@@ -0,0 +1,15 @@
+const mongoose=require('mongoose');
+const mongoURI="mongodb://localhost:27017";
+const connectToMongo=()=>{
+
+    mongoose.connect(mongoURI)
+        .then(()=>{
+        console.log("connected to mongo successfully");
+        }).catch((err)=>{
+            console.log("Failed to connect to mongo");
+            console.log(err);
+        })
+    }
+
+
+module.exports=connectToMongo;
